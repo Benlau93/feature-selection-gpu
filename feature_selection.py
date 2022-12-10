0472e9b94cd871a4f7c3bf3b01e3c0f771173b64
Benjamin Lau <ben_lau93@hotmail.com>
2022-12-10 16:14:38 +0800

    replace argsort with argmax

--- feature_selection.py
+++ feature_selection.py
@@ -24,7 +24,7 @@ def get_best_features(FEATURE_IMPT, X, y, reduce_ratio = 0.1):
         CV_RESULTS = np.append(CV_RESULTS, acc)
     
     # get number of feature that resulted in highest cv
-    num_feature = np.argsort(CV_RESULTS)[-1] + 1
+    num_feature = np.argmax(CV_RESULTS) + 1
     best_features_arg = FEATURE_IMPT_ARG[:num_feature]
     best_feature = FEATURE_IMPT[:num_feature]
 
