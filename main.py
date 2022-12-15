b76724ca63f3e5fdb540993dadec1878a2572aaf
BENJAMIN LAU <ben_lau93@hotmail.com>
2022-12-15 23:39:32 +0800

    add algo without knn/fs

--- main.py
+++ main.py
@@ -9,22 +9,40 @@ from feature_selection import algo1, algo2
 from svm import svm
 
 
-def get_prediction(X_train, y_train, nn_model, algo_f ,query):
-    # get nearest neighbour of a single query
-    nn = nn_model.get_NN([query])[0]
-    
-    # feature selection on query + nn
-    X_nn, y_nn = X_train[nn], y_train[nn]
-    if len(np.unique(y_nn)) == 1: # if all neighbour has the same class, no training needed
-        pred = [y_nn[0]]
-        best_features_arg, best_feature = [], []
+def get_prediction(X_train, y_train, nn_model, algo ,query):
+
+    # if knn
+    if "KNN" in algo:
+        # get nearest neighbour of a single query
+        nn = nn_model.get_NN([query])[0]
+        X_, y_ = X_train[nn], y_train[nn]
     else:
-        best_features_arg, best_feature = algo_f(X_nn, y_nn)
-        
-        # train classifier on best_features
-        X_nn_fs, query_fs = X_nn[:, best_features_arg], query[best_features_arg]
-        model = svm(X_nn_fs, y_nn)
-        pred = model.predict(query_fs.reshape(1,-1))
+        X_, y_ = X_train, y_train
+
+    # if all neighbour has the same class, no training needed
+    if len(np.unique(y_)) == 1:
+        pred = [y_[0]]
+        best_features_arg, best_feature = [], []
+    
+    else:
+        # if feature selection algo is selected
+        if "FS" in algo:
+            # run feature selection
+            algo_f = algo1 if "FS1" in algo else algo2
+            best_features_arg, best_feature = algo_f(X_, y_)
+            
+            # filter to best features
+            X_, query = X_[:, best_features_arg], query[:, best_features_arg]
+
+        else:
+            best_features_arg, best_feature = [], []
+
+
+        # train model on selected features
+        model = svm(X_, y_)
+        # make prediction
+        pred = model.predict(query.reshape(-1,X_.shape[1]))
+    
     
     return pred, best_features_arg, best_feature
 
@@ -39,31 +57,34 @@ def verbose_feature_impt(AVG_FEATURE_IMPT_ARG, AVG_FEATURE_IMPT, AVG_FEATURES):
 
 
 def main(method, data, algo, idx, top):
-    # determine algo used
-    algo_f = algo1 if algo == 1 else algo2
 
     # get data
     X_train, y_train,X_test, y_test  = load_mnist()
 
     # initialize feature impt
-    FEATURE_IMPT = np.zeros(X_train.shape[1])
+    num_feature = X_train.shape[1]
+    FEATURE_IMPT = np.zeros(num_feature)
     NUM_FEATURES = 0
     NUM_TEST = 0
 
     # track time
     start_time = time.time()
 
-    # train GENIE to get local NN classifer
-    nn_model = GENIE(X_train)
+    if "KNN" in algo:
+        # train GENIE to get local NN classifer
+        nn_model = GENIE(X_train)
+    else:
+        nn_model = None
+
+    # run method
     print()
-    # get overall test dataset acc
     if method == "test":
         print(f"--- Predicting on Index {idx} [Algorithm {algo}]---")
         # get query
-        query = X_test[idx]
+        query = X_test[idx].reshape(-1,num_feature)
 
         # get prediction
-        pred, best_features_arg, best_feature = get_prediction(X_train, y_train, nn_model, algo_f, query)
+        pred, best_features_arg, best_feature = get_prediction(X_train, y_train, nn_model, algo, query)
 
         # add to feature impt
         FEATURE_IMPT[best_features_arg] += best_feature
@@ -75,26 +96,36 @@ def main(method, data, algo, idx, top):
     elif method == "evaluate":
         
         print(f"--- Evaluating on {idx:,} Testing Data [Algorithm {algo}]---")
-        # initialize y_true and pred
-        y_true = np.zeros(0)
-        y_pred = np.zeros(0)
+        if "KNN" in algo:
+            # initialize y_true and pred
+            y_true = np.zeros(0)
+            y_pred = np.zeros(0)
 
-        for i in range(idx):
+            for i in range(idx):
+                # get query
+                query = X_test[i].reshape(-1,num_feature)
+
+                    # get prediction
+                    pred, best_features_arg, best_feature = get_prediction(X_train, y_train, nn_model, algo, query)
+
+                # add to feature impt
+                if len(best_features_arg) >0:
+                    FEATURE_IMPT[best_features_arg] += best_feature
+                    NUM_FEATURES += len(best_feature)
+                    NUM_TEST += 1
+
+                # add to y list
+                y_true = np.append(y_true, y_test[i])
+                y_pred = np.append(y_pred, pred)
+        
+        else:
+            # initialize y_true
+            y_true = y_test[:idx]
+
             # get query
-            query = X_test[i]
-
+            query = x_test[:idx].reshape(-1, num_feature)
             # get prediction
-            pred, best_features_arg, best_feature = get_prediction(X_train, y_train, nn_model, algo_f, query)
-
-            # add to feature impt
-            if len(best_features_arg) >0:
-                FEATURE_IMPT[best_features_arg] += best_feature
-                NUM_FEATURES += len(best_feature)
-                NUM_TEST += 1
-
-            # add to y list
-            y_true = np.append(y_true, y_test[i])
-            y_pred = np.append(y_pred, pred)
+            y_pred, best_features_arg, best_feature = get_prediction(X_train, y_train, nn_model, algo, query)
         
         # get classification metrics
         acc = accuracy_score(y_true, y_pred)
@@ -105,13 +136,14 @@ def main(method, data, algo, idx, top):
 
     end_time = time.time()
 
-    # verbose for top feature importance
-    AVG_FEATURE_IMPT = FEATURE_IMPT / NUM_TEST
-    AVG_FEATURES = NUM_FEATURES / NUM_TEST
-    AVG_FEATURE_IMPT_ARG = np.argsort(AVG_FEATURE_IMPT)[::-1][:top]
-    AVG_FEATURE_IMPT = AVG_FEATURE_IMPT[AVG_FEATURE_IMPT_ARG]
-    
-    verbose_feature_impt(AVG_FEATURE_IMPT_ARG, AVG_FEATURE_IMPT, AVG_FEATURES)
+    if "FS" in algo:
+        # verbose for top feature importance
+        AVG_FEATURE_IMPT = FEATURE_IMPT / NUM_TEST
+        AVG_FEATURES = NUM_FEATURES / NUM_TEST
+        AVG_FEATURE_IMPT_ARG = np.argsort(AVG_FEATURE_IMPT)[::-1][:top]
+        AVG_FEATURE_IMPT = AVG_FEATURE_IMPT[AVG_FEATURE_IMPT_ARG]
+        
+        verbose_feature_impt(AVG_FEATURE_IMPT_ARG, AVG_FEATURE_IMPT, AVG_FEATURES)
 
     print(f"Time Taken: {end_time - start_time:.04f}s")
     return 1
@@ -128,8 +160,8 @@ if __name__ == "__main__":
     # format arg
     try:
         idx = int(idx)
-        algo = int(algo)
         top = int(top)
+        algo = algo.upper()
     except ValueError:
         raise ValueError("Wrong format entered")
 
